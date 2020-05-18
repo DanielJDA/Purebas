a1db335fe96a8e5f047e220abfcbc37b1cd1dc78
DanielJDA <abrego@DESKTOP-40O0HLM.localdomain>
2020-05-18 09:58:18 -0600

    nuevo

---
+++ precache-manifest.0f425982a4508285dc0e445d999a6507.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "90bdd31f67756a7387d0a8231ad6b9c7",
+    "url": "./static/media/event.90bdd31f.svg"
+  },
+  {
+    "revision": "db41c1119dd42167a816",
+    "url": "./static/css/main.a4867659.chunk.css"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8daa6f7c6682b6c0aa89f9b591601012",
+    "url": "./static/media/grupo.8daa6f7c.png"
+  },
+  {
+    "revision": "79da948a311acb9c781e",
+    "url": "./static/js/2.c1bd3c12.chunk.js"
+  },
+  {
+    "revision": "6862d8f88a61243cbe5b2af1076caca6",
+    "url": "./static/media/linkedin.6862d8f8.svg"
+  },
+  {
+    "revision": "22f1db819699366f2655fbcbe786102a",
+    "url": "./static/media/facebook.22f1db81.svg"
+  },
+  {
+    "revision": "71c04586a57249821514c4158523a869",
+    "url": "./static/media/instagram.71c04586.svg"
+  },
+  {
+    "revision": "a35e76e7fadbbb8f72ff15aca5239d8a",
+    "url": "./static/media/particle.a35e76e7.png"
+  },
+  {
+    "revision": "b577b4cd37dbb4ea3cd3dcfd2b65159f",
+    "url": "./static/media/Grupo825.b577b4cd.svg"
+  },
+  {
+    "revision": "0b745203b989f8d25c391a2db6014195",
+    "url": "./static/media/aeroplane.0b745203.svg"
+  },
+  {
+    "revision": "1e9137175f6aba566683e674dd6f40c8",
+    "url": "./static/media/building.1e913717.svg"
+  },
+  {
+    "revision": "9c91a09d88d4e2f24180323408f11904",
+    "url": "./static/media/padlock.9c91a09d.svg"
+  },
+  {
+    "revision": "db41c1119dd42167a816",
+    "url": "./static/js/main.2c0da4fa.chunk.js"
+  },
+  {
+    "revision": "41357d8aff33b2bc6ff253ae7eac242d",
+    "url": "./static/media/spot.41357d8a.webm"
+  },
+  {
+    "revision": "793227ba61ab2511acbdb8bbce918fdd",
+    "url": "./static/media/MONO-RGOMODULAR-Bold.793227ba.ttf"
+  },
+  {
+    "revision": "8102ba86fa37119f11d783ac19ba32ce",
+    "url": "./static/media/MONO-RGOMODULAR-SemiBold.8102ba86.ttf"
+  },
+  {
+    "revision": "6a65a2be95ec73e531174ea7092332b2",
+    "url": "./static/media/MONO-RGOMODULAR-Regular.6a65a2be.ttf"
+  },
+  {
+    "revision": "c1ee866b35b0712a2bc0efc4d0cab3f3",
+    "url": "./static/media/bg4.c1ee866b.png"
+  },
+  {
+    "revision": "51ad0d9c15027618d3f8a6ef184441a6",
+    "url": "./static/media/bg1.51ad0d9c.png"
+  },
+  {
+    "revision": "1a78e597ad8b520f7bd47ac63d832ddf",
+    "url": "./static/media/bg10.1a78e597.png"
+  },
+  {
+    "revision": "b141d87fc7b9ccc6675de65cc41992e5",
+    "url": "./static/media/bg9.b141d87f.png"
+  },
+  {
+    "revision": "e99c9349b7f97836f884d5b29b15d3dc",
+    "url": "./static/media/bg7.e99c9349.png"
+  },
+  {
+    "revision": "4803cf6f1b35937265615244bf060567",
+    "url": "./static/media/bg5.4803cf6f.png"
+  },
+  {
+    "revision": "79da948a311acb9c781e",
+    "url": "./static/css/2.9da10ded.chunk.css"
+  },
+  {
+    "revision": "1276cf18e460744f302c4e8a09c4c17f",
+    "url": "./index.html"
+  }
+];
